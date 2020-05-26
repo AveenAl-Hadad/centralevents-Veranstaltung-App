6772025a68e5c9264b0673918c82b6cc56a50354
Andreas <a.schalla@swbmail.de>
2020-05-26 10:47:55 +0200

    Wrapper geupdated

--- Source/centralevents.Web.App/Wrapper/HttpClientWrapper.cs
+++ Source/centralevents.Web.App/Wrapper/HttpClientWrapper.cs
@@ -1,49 +1,66 @@
-﻿using System;
-using System.Diagnostics.CodeAnalysis;
-using System.Net.Http;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Components;
-
-namespace CentralEvents.Web.App.Wrappers
+﻿namespace CentralEvents.Web.App.Wrappers
 {
+	using System;
+	using System.Diagnostics.CodeAnalysis;
+	using System.Net.Http;
+	using System.Text;
+	using System.Threading.Tasks;
+
+	using Microsoft.AspNetCore.Components;
+
+	using Newtonsoft.Json;
+
 	[ExcludeFromCodeCoverage]
 	public class HttpClientWrapper : IHttpClient
 	{
-		private const string ApiUrl = "//localhost:54768/api/";
-		//private const string ApiUrl = "//localhost:50319/api/";
+		private static string ApiUrl = "//localhost:54768/";
+
+		//private const string ApiUrl = "//localhost:50319/";
 		private HttpClient httpClient;
 
 		public HttpClientWrapper()
 		{
 			this.httpClient = new HttpClient();
 		}
+		// CRUD:
 
+		// CREATE
+		public async Task<HttpResponseMessage> PostAsync<TModel>(string path, TModel model)
+		{
+			return await this.httpClient.PostAsync(this.GetUrl(path), this.JsonToStringContent(model));
+		}
+
+		//READ
 		public async Task<TModel> GetJsonAsync<TModel>(string path)
 		{
-			return await this.httpClient.GetJsonAsync<TModel>(ApiUrl + path);
+			return await this.httpClient.GetJsonAsync<TModel>(this.GetUrl(path));
 		}
 
-		public async Task<TModel> PutJsonAsync<TModel>(string path, object content)
+		//UPDATE
+		public async Task<HttpResponseMessage> PutAsync<TModel>(string path, TModel model)
 		{
-			return await this.httpClient.PutJsonAsync<TModel>(path, content);
+			return await this.httpClient.PutAsync(this.GetUrl(path), this.JsonToStringContent(model));
 		}
 
-		public async Task<TModel> PostJsonAsync<TModel>(string path, object content)
+		//DELETE
+		public async Task DeleteAsync(string path)
 		{
-			return await this.httpClient.PostJsonAsync<TModel>(path, content);
+			await this.httpClient.DeleteAsync(this.GetUrl(path));
 		}
 
-		public async Task DeleteJsonAsync<TModel>(string path)
-		{
-			await this.httpClient.DeleteAsync(path);
-		}
-
+		//Hilfsmethoden
 		private string GetUrl(string path)
 		{
 			Uri api = new Uri(ApiUrl);
 			Uri uri = new Uri(api, path);
-
 			return uri.ToString();
+		}
+
+		private StringContent JsonToStringContent(object content)
+		{
+			string serializedBody = JsonConvert.SerializeObject(content);
+			StringContent stringContent = new StringContent(serializedBody, Encoding.UTF8, "application/json");
+			return stringContent;
 		}
 	}
 }
---
+++ Source/centralevents.Web.App/Wrapper/IHttpClient.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+
+namespace CentralEvents.Web.App.Wrappers
+{
+	using System.Net.Http;
+
+	public interface IHttpClient
+	{
+		// CREATE
+		Task<HttpResponseMessage> PostAsync<TModel>(string path, TModel model);
+
+		//READ
+		Task<TModel> GetJsonAsync<TModel>(string path);
+
+		//UPDATE
+		Task<HttpResponseMessage> PutAsync<TModel>(string path, TModel model);
+		
+		//DELETE
+		Task DeleteAsync(string path);
+	}
+}
