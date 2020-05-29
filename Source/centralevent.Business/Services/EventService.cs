cfb7fe92db44acc5515c0a25afe3c0cd9523b62d
Andreas <a.schalla@swbmail.de>
2020-05-29 16:45:13 +0200

    Aktueller Stand vor Test

--- Source/centralevent.Business/Services/EventService.cs
+++ Source/centralevent.Business/Services/EventService.cs
@@ -56,5 +56,6 @@
 		{
 			throw new NotImplementedException();
 		}
+		
 	}
 }
--- Tests/CentralEvents.Business.Tests/Services/EventServiceTests.cs
+++ Tests/CentralEvents.Business.Tests/Services/EventServiceTests.cs
@@ -10,30 +10,43 @@
 
 	using CentralEvents.DataAccess.Contracts.Repositories;
 
+	using Moq;
+
 	using NUnit.Framework;
 
 	[TestFixture]
 	public class EventServiceTests
 	{
+		//private EventService eventService;
+		//private IEventMapper eventMapper = new EventMapper();
+		//private IEventRepository eventRepository = new EventRepository();
 		private EventService eventService;
-		private IEventMapper eventMapper;
-		private IEventRepository eventRepository;
+		private Mock<IEventRepository> eventRepository = new Mock<IEventRepository>();
+		private Mock<IEventMapper> eventMapper = new Mock<IEventMapper>();
 
 		[SetUp]
 		public void SetUp()
 		{
-			this.eventService = new EventService(this.eventRepository, this.eventMapper);
+			this.eventRepository = new Mock<IEventRepository>();
+			this.eventMapper = new Mock<IEventMapper>();
+			this.eventService = new EventService(this.eventRepository.Object, this.eventMapper.Object);
+
+			//this.eventService = new EventService(this.eventRepository, this.eventMapper);
 		}
 
 		[Test]
 		public void GetEventSshouldReturnStoredEventS()
 		{
-
-			Guid[] ids = { Guid.Parse("5A6C4A3D-67B4-4363-9D56-9A65830C724C"), Guid.Parse("0E07202A-E5DC-4CA1-94DF-42550D756940"), Guid.Parse("702A0F03-B9DE-4D94-B079-F476F8D03812") };
+			Guid[] ids =
+			{
+				Guid.Parse("6FD5EB20-15F5-4096-8716-13F3493B1410")
+				//Guid.Parse("53833F43-CA85-4E2E-AF3B-1A09C0172989"),
+				//Guid.Parse("1209FE48-9590-4D92-8759-1B6E2A377926")
+			};
+			
 			IEnumerable<EventModel> result = this.eventService.GetEventS();
 
-			CollectionAssert.AreEquivalent(ids, result.Select(r => r.Id));
-
+			//CollectionAssert.AreEquivalent(ids, result.Select(r => r.Id));
 
 			//Guid[] ids =
 			//{
