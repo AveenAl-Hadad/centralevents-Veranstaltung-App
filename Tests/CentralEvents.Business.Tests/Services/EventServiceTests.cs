01dc68a3b92a548b7153cea09a37b20eb6cad539
Lord KWendt <lord.kwendt@gmail.com>
2020-05-29 10:08:50 +0200

    TestStruktur angelegt und Grundtests eingefügt

---
+++ Tests/CentralEvents.Business.Tests/Services/EventServiceTests.cs
@@ -0,0 +1,50 @@
+﻿namespace CentralEvents.Business.Tests.Services
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+
+	using CentralEvent.Business.Contracts.Mappers;
+	using CentralEvent.Business.Contracts.Models;
+	using CentralEvent.Business.Services;
+
+	using CentralEvents.DataAccess.Contracts.Repositories;
+
+	using NUnit.Framework;
+
+	[TestFixture]
+	public class EventServiceTests
+	{
+		private EventService eventService;
+		private IEventMapper eventMapper;
+		private IEventRepository eventRepository;
+
+		[SetUp]
+		public void SetUp()
+		{
+			this.eventService = new EventService(this.eventRepository, this.eventMapper);
+		}
+
+		[Test]
+		public void GetEventSshouldReturnStoredEventS()
+		{
+
+			Guid[] ids = { Guid.Parse("5A6C4A3D-67B4-4363-9D56-9A65830C724C"), Guid.Parse("0E07202A-E5DC-4CA1-94DF-42550D756940"), Guid.Parse("702A0F03-B9DE-4D94-B079-F476F8D03812") };
+			IEnumerable<EventModel> result = this.eventService.GetEventS();
+
+			CollectionAssert.AreEquivalent(ids, result.Select(r => r.Id));
+
+
+			//Guid[] ids =
+			//{
+			//	Guid.Parse("5A6C4A3D-67B4-4363-9D56-9A65830C724C"),
+			//	Guid.Parse("0E07202A-E5DC-4CA1-94DF-42550D756940"),
+			//	Guid.Parse("702A0F03-B9DE-4D94-B079-F476F8D03812")
+			//};
+
+			//IEnumerable<EventModel> result = this.eventService.GetEventS();
+
+			//CollectionAssert.AreEquivalent(ids, result.Select(t => t.Id));
+		}
+	}
+}
---
+++ Tests/CentralEvents.Web.Api.Tests/Controllers/CentralEventsControllerTests.cs
@@ -0,0 +1,42 @@
+﻿namespace CentralEvents.Web.Api.Tests.Controllers
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+
+	using CentralEvent.Business.Contracts.Models;
+	using CentralEvent.Business.Contracts.Services;
+
+	using CentralEvents.Web.Api.Controllers;
+
+	using Moq;
+
+	using NUnit.Framework;
+
+	[TestFixture]
+	public class CentralEventsControllerTests
+	{
+		private CentralEventsController cEcontroller;
+		private Mock<IEventService> eventService;
+
+		[SetUp]
+		public void SetUp()
+		{
+			this.eventService = new Mock<IEventService>();
+
+			this.cEcontroller = new CentralEventsController(this.eventService.Object);
+		}
+
+		[Test]
+		public void GetEventsShouldDelegateToEventService()
+		{
+			List<EventModel> eventS = Enumerable.Range(1, 2).Select(id => new EventModel { Id = new Guid() }).ToList();
+
+			this.eventService.Setup(t => t.GetEventS()).Returns(eventS);
+
+			IEnumerable<EventModel> result = this.cEcontroller.GetEventS();
+
+			CollectionAssert.AreEquivalent(eventS, result);
+		}
+	}
+}
