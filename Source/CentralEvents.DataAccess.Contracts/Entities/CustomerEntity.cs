b8e73446956b3535d6bd6fff3d5748c8cd7c7394
Lord KWendt <lord.kwendt@gmail.com>
2020-06-02 11:41:19 +0200

    ITBH7-51 Tabelle und Entity für Registrierung anlegen

---
+++ Source/CentralEvents.DataAccess.Contracts/Entities/CustomerEntity.cs
@@ -0,0 +1,32 @@
+﻿namespace CentralEvents.DataAccess.Contracts.Entities
+{
+	using System;
+
+	public class CustomerEntity : EntityBase
+	{
+		public CustomerEntity()
+		{
+			this.Id = Guid.NewGuid();
+		}
+
+		public string Vorname { get; set; }
+
+		public string Nachname { get; set; }
+
+		public string Strasse { get; set; }
+
+		public string Hausnummer { get; set; }
+
+		public string Plz { get; set; }
+
+		public string Ort { get; set; }
+
+		public string Telefon { get; set; }
+
+		public string Email { get; set; }
+
+		public string Benutzername { get; set; }
+
+		public string Passwort { get; set; }
+	}
+}
--- Source/CentralEvents.DataAccess/Migrations/20200602092202_CustomerTable.Designer.cs
+++ Source/CentralEvents.DataAccess/Migrations/20200602092202_CustomerTable.Designer.cs
@@ -4,14 +4,16 @@ using CentralEvents.DataAccess.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CentralEvents.DataAccess.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20200602092202_CustomerTable")]
+    partial class CustomerTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
