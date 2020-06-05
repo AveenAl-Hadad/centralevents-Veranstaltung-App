a7a4905cd74bdbbd0028596985a9201dedb9786d
Lord KWendt <lord.kwendt@gmail.com>
2020-06-05 10:22:40 +0200

    gitflow-feature-stash: UserStory77-ControllerNachFachlichkeitAufbrechen

---
+++ Source/centralevents.Web.Api/Controllers/EventController.cs
@@ -0,0 +1,49 @@
+﻿namespace CentralEvents.Web.Api.Controllers
+{
+	using System;
+	using System.Collections.Generic;
+
+	using CentralEvent.Business.Contracts.Models;
+	using CentralEvent.Business.Contracts.Services;
+
+	using Microsoft.AspNetCore.Mvc;
+
+	[Route("api/event")]
+	public class EventController : ControllerBase
+	{
+		private readonly IEventService eventService;
+
+		public EventController(IEventService eventService)
+		{
+			this.eventService = eventService;
+		}
+
+		[Route("add")]
+		[HttpPost]
+		public void AddEvent([FromBody] EventModel eventModel) // FROMBODY voll wichtig da das Model in HTTP gewrappt ist und so!!!!
+		{
+			this.eventService.AddEvent(eventModel);
+		}
+
+		[Route("getAll")]
+		[HttpGet]
+		public IEnumerable<EventModel> GetEventS()
+		{
+			return this.eventService.GetEventS();
+		}
+
+		[Route("get/{id}")]
+		[HttpGet]
+		public EventModel GetEvent(Guid id)
+		{
+			return this.eventService.GetEvent(id);
+		}
+
+		[Route("update")]
+		[HttpPut]
+		public void UpdateEvent([FromBody] EventModel eventModel)
+		{
+			this.eventService.UpdateEvent(eventModel);
+		}
+	}
+}
